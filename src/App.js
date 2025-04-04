dd99a3d6c0afbc840a3086e96462b489f4b770da
Prajwal-Nayak-S <prajwalnayaks772@gmail.com>
2025-04-04 22:13:46 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import LearningComponent from './components/learning-examples/learningComponent';
+
+function App() {
+  return (
+    <div className="App">
+     
+    <LearningComponent/>
+    </div>
+    
+  );
+}
+
+
+
+
+export default App;
